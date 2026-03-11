cfd85d3ba6450024dec50567cfc9548256a879d4
Chaitali Mhetras <118844690+Chaitali1105@users.noreply.github.com>
2026-03-11 07:26:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const bookingsRoutes = require('./routes/bookings');
+const timetableRoutes = require('./routes/timetable');
 
 const app = express();
 const PORT = 5000;
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/', authRoutes);
 app.use('/', bookingsRoutes);
+app.use('/', timetableRoutes);
 
 // Health check endpoint
 app.get('/health', (req, res) => {
